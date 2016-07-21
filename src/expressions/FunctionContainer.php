3da73830692b5c67fe7a04c2fe631a256442fc12
bnjhope <ben3@lougeek.com>
2016-07-21 14:10:25 -0400

    function container class created

---
+++ src/expressions/FunctionContainer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Owner
+ * Date: 21/07/2016
+ * Time: 13:52
+ */
+
+namespace contour\parser\expressions;
+
+
+class FunctionContainer
+{
+
+    /**
+     * The tree representation of the saved function.
+     * @var iExpression[]
+     */
+    private $functionTree;
+
+    /**
+     *
+     */
+
+}
