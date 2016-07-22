2019c0dc38f8a9267dd76ef0c49dcf12eef0ed50
bnjhope <ben3@lougeek.com>
2016-07-22 10:05:08 -0400

    updated function call parser

--- src/parsers/FunctionCallParser.php
+++ src/parsers/FunctionCallParser.php
@@ -41,7 +41,23 @@ class FunctionCallParser
         $this->stack = new ParseStack();
     }
 
-    public function parse() {
+    public function parse($expr) {
+
+        //set this objects expression to the expression passed to the parse function so that all properties and methods
+        //of the object can access the expression.
+        $this->expr = $expr;
+
+        //split the expression into an array of characters
+        $this->exprArray = str_split($this->expr);
+
+        //set the parse index to the beginning of the expression
+        $this->parseIndex = 0;
+
+        //test to see that the keyword is "func", if it is not then it is not a function being parsed.
+        $keyword = $this->getUptoHashtag();
+    }
+
+    public function getUptoHashtag() {
 
     }
 
