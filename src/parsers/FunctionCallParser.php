04becad9da8f88b8a5eb64411e51e7b024249dd2
bnjhope <bnjhope1@gmail.com>
2016-07-22 14:28:11 -0400

    added comments and PHPDocs

--- src/parsers/FunctionCallParser.php
+++ src/parsers/FunctionCallParser.php
@@ -69,53 +69,78 @@ class FunctionCallParser
         //split the expression into an array of characters
         $this->exprArray = str_split($this->expr);
 
-
     }
 
+    /**
+     * Returns the contents of string that has comma separated values within brackets.
+     * @return array
+     * The strings separated by the commas within the brackets.
+     * @throws ExpressionParseException
+     * If the expression is syntactically incorrect, then this expression is thrown.
+     */
     function parseCommaSeparatedBrackets() {
 
         /**
+         * The current character being looked at by the parser.
          * @var string
          */
         $currentChar = "";
 
         /**
+         * Determines if the parser has found a close bracket or not yet while parsing the expression.
+         * If it has found one, this variable turns true and the parser loop ends.
          * @var bool
          */
         $closeBracketFound = false;
 
         /**
-         *
+         * Holds a string of characters of a result to be added to the results array.
+         * @var string
          */
         $strToAdd = "";
 
         /**
+         * The array of results of strings to be returned to the calling function.
          * @var string[]
          */
         $result = array();
 
+        //it is a syntax error if no open bracket is found at the start. If there is none found, then
+        //throw this exception.
         if($this->getNextChar() != "(") {
             throw new ExpressionParseException("Open bracket not found");
         }
 
+        //while the close bracket has not been found and while the
+        //there are still characters to be parsed in the stream
         while(!$closeBracketFound && $this->hasNext()) {
 
+            //set the current character as the next character
+            //in the stream
             $currentChar = $this->getNextChar();
 
+            //determine what to do based on what the current character is
             switch($currentChar) {
+
+                //if its a comma, add the new value to the array
                 case "," :
                     array_push($result, trim($strToAdd));
                     $strToAdd = "";
                     break;
+
+                //if its a close bracket, add the last value and exit the loop
                 case ")" :
                     array_push($result, trim($strToAdd));
                     $closeBracketFound = true;
                     break;
+
+                //otherwise, just add the character to the current string being read
                 default :
                     $strToAdd .= $currentChar;
             }
         }
 
+        //return the array of results
         return $result;
     }
 
